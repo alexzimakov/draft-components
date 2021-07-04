b416f1e5024fc2e18c7a4392c21ac35e231d7a23
Alexander Zimakov <alexzimakov@icloud.com>
2021-07-04 16:55:55 +0300

    Change `Spinner` look

--- src/components/spinner/spinner.tsx
+++ src/components/spinner/spinner.tsx
@@ -24,18 +24,13 @@ export function Spinner({
       width={24}
       height={24}
     >
-      <circle
-        className="dc-spinner__circle"
-        stroke="currentColor"
-        strokeWidth={4}
-        cx={12}
-        cy={12}
-        r={10}
-      />
       <path
-        className="dc-spinner__arc"
-        fill="currentColor"
-        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M1,12 A11,11 0 1 0 12,1"
       />
     </svg>
   );
