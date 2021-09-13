ed1db2f174de39d88d3ddf373d273f00ec7dbbb9
Alexander Zimakov <alexzimakov@icloud.com>
2021-09-13 11:02:18 +0300

    `LoadingView` - small improvements

--- src/components/loading-view/loading-view.tsx
+++ src/components/loading-view/loading-view.tsx
@@ -1,15 +1,17 @@
 import { ComponentPropsWithoutRef } from 'react';
 import { classNames } from '../../lib/react-helpers';
-import { Spinner } from '../spinner';
+import { Spinner, SpinnerProps } from '../spinner';
 
 export type LoadingViewHtmlAttrs = ComponentPropsWithoutRef<'div'>;
 
 export interface LoadingViewProps extends LoadingViewHtmlAttrs {
   padY?: 'none' | '2xs' | 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl';
+  spinnerSize?: SpinnerProps['size'];
 }
 
 export function LoadingView({
   padY = '2xl',
+  spinnerSize,
   className,
   children,
   ...props
@@ -23,7 +25,7 @@ export function LoadingView({
         `dc-loading-view_pad-y_${padY}`
       )}
     >
-      <Spinner className="dc-loading-view__spinner" size="1.75em" />
+      <Spinner className="dc-loading-view__spinner" size={spinnerSize} />
       <div className="dc-loading-view__content">{children}</div>
     </div>
   );
