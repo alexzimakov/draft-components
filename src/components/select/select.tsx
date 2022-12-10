9027a1857dab7c014b5cb1234c09f5334cb346fb
Alexander Zimakov <alexzmdev@gmail.com>
2022-12-10 16:34:21 +0700

    Update `Select` icon

--- src/components/select/select.tsx
+++ src/components/select/select.tsx
@@ -82,12 +82,10 @@ export const Select = forwardRef<HTMLSelectElement, SelectProps>(
           height={24}
           fill="none"
           stroke="currentColor"
-          strokeWidth={3}
+          strokeWidth={1.5}
+          aria-hidden={true}
         >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-          />
+          <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 15L12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
         </svg>
       </div>
     );
