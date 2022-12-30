2c18530943f56a5372829351a8f5cdf7c5268600
Alexander Zimakov <alexzmdev@gmail.com>
2022-12-30 14:15:31 +0700

    Update `Button` component

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -10,6 +10,7 @@ export type ButtonRenderFn = (props: {
   children: JSX.Element;
 }) => JSX.Element;
 export type ButtonProps = ComponentPropsWithRef<'button'> & {
+  isBlock?: boolean;
   loading?: boolean;
   size?: ButtonSize,
   variant?: ButtonVariant;
@@ -19,80 +20,84 @@ export type ButtonProps = ComponentPropsWithRef<'button'> & {
   renderAs?: ButtonRenderFn;
 };
 
-export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-  function Button({
-    disabled = false,
-    loading = false,
-    type = 'button',
-    size = 'sm',
-    variant = 'filled',
-    appearance = 'default',
-    className = '',
-    icon = null,
-    caption = null,
-    children = null,
-    renderAs,
-    ...props
-  }, ref) {
-    const shouldRenderIcon = Boolean(icon);
-    const shouldRenderLabel = Boolean(children);
-    const shouldRenderCaption = Boolean(caption);
+export const Button = forwardRef<
+  HTMLButtonElement,
+  ButtonProps
+>(function Button({
+  isBlock = false,
+  disabled = false,
+  loading = false,
+  type = 'button',
+  size = 'sm',
+  variant = 'filled',
+  appearance = 'default',
+  icon = null,
+  caption = null,
+  renderAs,
+  className,
+  children,
+  ...props
+}, ref) {
+  const shouldRenderIcon = Boolean(icon);
+  const shouldRenderLabel = Boolean(children);
+  const shouldRenderCaption = Boolean(caption);
 
-    let leadingAddOn: ReactNode = null;
-    if (loading) {
-      leadingAddOn = (
-        <span data-testid="button-spinner" className="dc-button__spinner">
-          <Spinner size="1.15em" />
-        </span>
-      );
-    } else if (shouldRenderIcon) {
-      leadingAddOn = (
-        <span data-testid="button-icon" className="dc-button__icon">
-          {icon}
-        </span>
-      );
-    }
-
-    const content = (
-      <>
-        {leadingAddOn}
-        {shouldRenderLabel && (
-          <span className="dc-button__body">
-            <span className="dc-button__label">{children}</span>
-            {shouldRenderCaption && (
-              <small className="dc-button__caption">{caption}</small>
-            )}
-          </span>
-        )}
-      </>
+  let leadingAddOn: ReactNode = null;
+  if (loading) {
+    leadingAddOn = (
+      <span data-testid="button-spinner" className="dc-button__spinner">
+        <Spinner size="1.15em" />
+      </span>
     );
-
-    className = classNames('dc-button', className, {
-      [`dc-button_${size}`]: size !== undefined,
-      [`dc-button_appearance_${appearance}`]: appearance !== undefined,
-      [`dc-button_variant_${variant}`]: variant !== undefined,
-      'dc-button_loading': loading,
-      'dc-button_has_icon': shouldRenderIcon,
-      'dc-button_has_caption': shouldRenderCaption,
-    });
-
-    if (typeof renderAs === 'function') {
-      return renderAs({
-        className,
-        children: content,
-      });
-    }
-
-    return (
-      <button
-        ref={ref}
-        type={type}
-        disabled={disabled || loading}
-        className={className}
-        {...props}
-      >
-        {content}
-      </button>
+  } else if (shouldRenderIcon) {
+    leadingAddOn = (
+      <span data-testid="button-icon" className="dc-button__icon">
+        {icon}
+      </span>
     );
   }
-);
+
+  const content = (
+    <>
+      {leadingAddOn}
+      {shouldRenderLabel && (
+        <span className="dc-button__body">
+          <span className="dc-button__label">{children}</span>
+          {shouldRenderCaption && (
+            <small className="dc-button__caption">{caption}</small>
+          )}
+        </span>
+      )}
+    </>
+  );
+
+  className = classNames(className, {
+    'dc-button': true,
+    'dc-button_block': isBlock,
+    'dc-button_loading': loading,
+    'dc-button_has_icon': shouldRenderIcon,
+    'dc-button_has_caption': shouldRenderCaption,
+    [`dc-button_${size}`]: size,
+    [`dc-button_appearance_${appearance}`]: appearance,
+    [`dc-button_variant_${variant}`]: variant,
+  });
+
+  if (typeof renderAs === 'function') {
+    return renderAs({
+      className,
+      children: content,
+    });
+  }
+
+  return (
+    <button
+      ref={ref}
+      type={type}
+      disabled={disabled || loading}
+      className={className}
+      {...props}
+    >
+      {content}
+    </button>
+  );
+});
