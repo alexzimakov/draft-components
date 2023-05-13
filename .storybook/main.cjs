015a489cd46dd9f85d157fa5fa82506d711e58b2
Alexander Zimakov <alexzmdev@gmail.com>
2023-05-13 12:24:12 +0300

    Update storybook

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -3,18 +3,16 @@
  */
 const config = {
   core: {
-    builder: '@storybook/builder-vite',
-    disableTelemetry: true,
+    disableTelemetry: true
   },
-  stories: [
-    '../src/**/*.stories.mdx',
-    '../src/**/*.stories.@(js|jsx|ts|tsx)',
-  ],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-  ],
-  framework: '@storybook/react',
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {}
+  },
+  docs: {
+    autodocs: true
+  }
 };
-
 module.exports = config;
