1f1b91db5ab8a4bdf1b35648fcee2a03234b778f
Alexander Zimakov <alexzmdev@gmail.com>
2023-05-13 16:52:30 +0300

    Storybook: fix theme select

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,49 +3,65 @@ import '../src/components/index.css';
 import '../src/components/index.dark.css';
 import './preview.css';
 
-export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-  backgrounds: {
-    disable: true,
-  },
-  grid: {
-    cellSize: 16,
-  },
-};
-
-export const globalTypes = {
-  theme: {
-    name: 'Theme',
-    description: 'Global theme for components',
-    defaultValue: 'light',
-    toolbar: {
-      icon: 'photo',
-      items: [
-        { value: 'light', title: 'Light' },
-        { value: 'dark', title: 'Dark' },
-      ],
-      title: true,
-      dynamicTitle: true,
-    },
-  },
-};
-
 const withTheme = (storyFn, context) => {
+  const viewMode = context.viewMode;
   const theme = context.globals.theme || 'light';
 
   useLayoutEffect(() => {
-    document.body.classList.add(theme);
+    const className = 'dc-story-doc';
+    const body = document.body;
+    const docs = document.querySelectorAll('.docs-story');
+
+    body.classList.add(theme);
+    for (const doc of docs) {
+      doc.classList.add(className);
+    }
     return () => {
-      document.body.classList.remove(theme);
+      body.classList.remove(theme);
+      for (const doc of docs) {
+        doc.classList.remove(className);
+      }
     };
-  }, [theme]);
+  }, [viewMode, theme]);
 
   return storyFn();
 };
-export const decorators = [withTheme];
+
+const preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+    backgrounds: {
+      disable: true,
+    },
+    grid: {
+      cellSize: 16,
+    },
+  },
+
+  globalTypes: {
+    theme: {
+      name: 'Theme',
+      description: 'Global theme for components',
+      defaultValue: 'light',
+      toolbar: {
+        icon: 'photo',
+        items: [
+          { value: 'light', title: 'Light' },
+          { value: 'dark', title: 'Dark' },
+        ],
+        title: true,
+        dynamicTitle: true,
+      },
+    },
+  },
+
+  decorators: [withTheme],
+};
+
+export default preview;
