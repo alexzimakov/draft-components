f01fe746dc70bb3433cdbdb48551cf95485677e7
Alexander Zimakov <alexzmdev@gmail.com>
2023-08-04 15:41:22 +0300

    Update Radio component

--- src/components/radio/radio.tsx
+++ src/components/radio/radio.tsx
@@ -1,4 +1,4 @@
-import { ComponentPropsWithRef, JSX, forwardRef } from 'react';
+import { ComponentPropsWithRef, ComponentPropsWithoutRef, JSX, forwardRef } from 'react';
 import { classNames } from '../../lib/react-helpers';
 
 type RadioHTMLProps = ComponentPropsWithRef<'input'>;
@@ -10,54 +10,21 @@ export type RadioProps = RadioBaseProps & {
   onToggle?: RadioToggleHandler;
 };
 
-const checkIcon = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    width="24"
-    height="24"
-    className="dc-radio__icon"
-    data-testid="radio-check-icon"
-  >
-    <path
-      d="M7 12.7222L11 16.5L17 8"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2.5"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
-  </svg>
-);
-
-const dotIcon = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    width="24"
-    height="24"
-    className="dc-radio__icon"
-    data-testid="radio-dot-icon"
-  >
-    <circle cx="12" cy="12" r="5" fill="currentColor" />
-  </svg>
-);
-
 export const Radio = forwardRef<HTMLInputElement, RadioProps>(function Radio({
   icon = 'dot',
-  style = {},
-  className = '',
+  style,
+  className,
   onChange,
   onToggle,
   ...props
 }, ref) {
-  let renderedIcon: JSX.Element;
+  let iconElement: JSX.Element;
   switch (icon) {
     case 'check':
-      renderedIcon = checkIcon;
+      iconElement = <CheckIcon className="dc-radio__icon" data-testid="radio-dot-icon" />;
       break;
     default:
-      renderedIcon = dotIcon;
+      iconElement = <DotIcon className="dc-radio__icon" data-testid="radio-check-icon" />;
   }
 
   return (
@@ -77,8 +44,48 @@ export const Radio = forwardRef<HTMLInputElement, RadioProps>(function Radio({
         data-testid="radio-check"
         aria-hidden={true}
       >
-        {renderedIcon}
+        {iconElement}
       </span>
     </label>
   );
 });
+
+function CheckIcon(props: ComponentPropsWithoutRef<'svg'>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+      width={20}
+      height={20}
+      {...props}
+    >
+      <path
+        d="M5 10.4444L9 14L15 6"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+function DotIcon(props: ComponentPropsWithoutRef<'svg'>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+      width={20}
+      height={20}
+      {...props}
+    >
+      <circle
+        cx={10}
+        cy={10}
+        r={4}
+        fill="currentColor"
+      />
+    </svg>
+  );
+}
