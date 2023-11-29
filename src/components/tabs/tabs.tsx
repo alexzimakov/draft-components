5a31179ff527c3f1da70b0011cd7b33a6a57b6f5
Alexander Zimakov <alexzmdev@gmail.com>
2023-11-29 13:52:57 +0700

    Tabs sub components

--- src/components/tabs/tabs.tsx
+++ src/components/tabs/tabs.tsx
@@ -2,6 +2,9 @@ import { ComponentPropsWithoutRef } from 'react';
 import { TabName, TabSetter } from './types.js';
 import { classNames } from '../../lib/react-helpers.js';
 import { TabsContextProvider } from './tabs-context.js';
+import { TabList } from './tab-list.js';
+import { Tab } from './tab.js';
+import { TabPanel } from './tab-panel.js';
 
 export interface TabsProps extends ComponentPropsWithoutRef<'div'> {
   selectedTab: TabName;
@@ -26,3 +29,6 @@ export function Tabs({
     </TabsContextProvider>
   );
 }
+Tabs.List = TabList;
+Tabs.Tab = Tab;
+Tabs.Panel = TabPanel;
