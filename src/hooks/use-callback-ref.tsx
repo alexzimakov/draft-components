9f43f9f0fef8f3d053dc02a97a695bc11aef1381
Alexander Zimakov <alexzmdev@gmail.com>
2023-12-12 14:02:09 +0700

    Fix useCallbackRef types

--- src/hooks/use-callback-ref.tsx
+++ src/hooks/use-callback-ref.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useMemo, useRef } from 'react';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-type Callback = (...args: unknown[]) => unknown;
+type Callback = (...args: any[]) => any;
 
 export function useCallbackRef<T extends Callback>(callback: T): T {
   const savedCallback = useRef(callback);
