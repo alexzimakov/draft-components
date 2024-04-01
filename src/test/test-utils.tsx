6f506b880e35fc8b8d5be18a0e0e7c62587937d9
Alexander Zimakov <alexzmdev@gmail.com>
2024-04-01 13:57:39 +0700

    Update `test-utils`

--- src/test/test-utils.tsx
+++ src/test/test-utils.tsx
@@ -1,5 +1,2 @@
-import userEventPkg from '@testing-library/user-event';
-
-// https://github.com/testing-library/user-event/issues/1146
-export const userEvent = userEventPkg as unknown as (typeof userEventPkg)['default'];
 export * from '@testing-library/react';
+export { userEvent } from '@testing-library/user-event';
