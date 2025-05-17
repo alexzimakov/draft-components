3591234b13cb65834e4e9bd5b73740c30e9d9b1f
Alexander Zimakov <alexzmdev@gmail.com>
2025-05-17 14:13:05 +0300

    Delete `SlideOver` component

--- src/components/slide-over/types.ts
+++
@@ -1,4 +0,0 @@
-export type SlideOverCloseCallback = (source?:
-  | 'close-button'
-  | 'backdrop'
-  | 'escape') => void;
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,7 +1,5 @@
 export * from './use-ref-callback.js';
 export * from './use-lock-body-scroll.js';
-export * from './use-esc-key-down.js';
 export * from './use-focus-trap.js';
 export * from './use-mount-transition.js';
-export * from './use-show-transition.js';
 export * from './use-preserve-props-when-closed.js';
