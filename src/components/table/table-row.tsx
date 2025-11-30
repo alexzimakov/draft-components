c02ae4af115f7bee244d5727eef5f98975903342
Alexander Zimakov <alexzmdev@gmail.com>
2025-11-30 20:55:29 +0300

    `TableRow`: add `shouldNotHighlightOnHover` property

--- src/components/table/table-row.tsx
+++ src/components/table/table-row.tsx
@@ -4,6 +4,7 @@ type TableRowHTMLProps = ComponentProps<'tr'>;
 
 type TableRowBaseProps = {
   isSelected?: boolean;
+  shouldNotHighlightOnHover?: boolean;
 };
 
 export type TableRowProps =
@@ -12,12 +13,14 @@ export type TableRowProps =
 
 export function TableRow({
   isSelected,
+  shouldNotHighlightOnHover,
   ...props
 }: TableRowProps) {
   return (
     <tr
       {...props}
       data-selected={isSelected}
+      data-not-highlight-on-hover={shouldNotHighlightOnHover}
     />
   );
 }
